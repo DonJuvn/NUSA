ba975d21b72def6e57ccea9ac88f51e5e1f0a61c
DonJuan <armvnov1ch@gmail.com>
2024-11-19 00:11:56 +0500

    about section

---
+++ src/components/about.jsx
@@ -0,0 +1,26 @@
+import React from "react";const About = () => {
+   return (
+      <div id="about">
+         <div className="container">
+            <div className="about">
+               <div className="welcome">
+                  <h1>
+                     Добро пожаловать в нашу цифровую лабораторию, где мы
+                     превращаем ваши идеи в инновационные IT-продукты!
+                  </h1>
+                  <a id='cta' href="#">
+                     Давайте начнем!
+                     <img src="img/arrow-link.png" alt="" />
+                  </a>
+                  <a id="arrow-down" href="#">
+                     <img src="img/arrow-down.png" alt="" />
+                  </a>
+               </div>
+               <div className="white-bg"></div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default About;
