9e2157b119d359cba5157af46995b4e76d29d0e1
DonJuan <armvnov1ch@gmail.com>
2024-11-19 22:53:03 +0500

    href links

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const Menu = () => {
-   return(
+import React from "react";const Menu = () => {
+   return (
       <div id="menu">
          <div className="container">
             <div className="menu">
@@ -9,18 +7,24 @@ const Menu = () => {
                   <img src="img/logo.png" alt="" />
                </div>
                <div className="navbar">
-                  <a id='white' href="#">Главная</a>
-                  <a id='white' href="#">Про нас</a>
-                  <a id='white' href="#">Кейсы</a>
-                  <a href="#">Услуги</a>
-                  <a href="#">Отзывы</a>
-                  <a href="#">Сотрудники</a>
-                  <a href="#">Контакты</a>
+                  <a id="white" href="#about">
+                     Главная
+                  </a>
+                  <a id="white" href="#counters">
+                     Про нас
+                  </a>
+                  <a id="white" href="#cases">
+                     Кейсы
+                  </a>
+                  <a href="#services">Услуги</a>
+                  <a href="#feedbacks">Отзывы</a>
+                  <a href="#employees">Сотрудники</a>
+                  <a href="#contacts">Контакты</a>
                </div>
             </div>
          </div>
       </div>
-   )
-}
+   );
+};
 
 export default Menu;
