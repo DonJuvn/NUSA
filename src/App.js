284694dd6141a7cd759eb5204b1157a77c8b93de
DonJuan <armvnov1ch@gmail.com>
2024-11-25 03:11:45 +0500

    feedbacks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.svg";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import Menu from "./components/menu";
 import About from "./components/about";
 import Counters from "./components/counters";
 import Cases from "./components/cases";
 import Services from "./components/services";
+import Feedbacks from "./components/feedbacks";
 // import ShadowBox from "./components/shadow-box";
 // import MouseShadow from "./components/MouseShadow";
+
+const members = [
+   { avatar: "img/avatar1.png", name: "Member 2" },
+   { avatar: "img/avatar2.png", name: "Member 1" },
+   { avatar: "img/avatar3.png", name: "Member 3" },
+   { avatar: "img/avatar4.png", name: "Member 4" },
+   { avatar: "img/avatar5.png", name: "Member 5" },
+];
 
 function App() {
    return (
@@ -17,9 +28,11 @@ function App() {
          <Counters />
          <Cases />
          <Services />
+         <Feedbacks members={members} />
          {/* <ShadowBox /> */}
       </div>
    );
 }
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
--- src/components/feedbacks.jsx
+++ src/components/feedbacks.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React from "react";const Feedbacks = ({ members }) => {
+   const predefinedAngles = [90, 120, 180, 240, 270];
+   const getRandomInRange = (min, max) => Math.random() * (max - min) + min;
 
-const Feedbacks = () => {
-   return(
-      <div id="feedbacks">
-         <h1 id="title"></h1>
+   return (
+      <div className="orbit-container">
+         {/* Sun in the center */}
+         <div className="sun"></div>
+
+         {/* Orbits */}
+         {[...Array(4)].map((_, orbitIndex) => {
+            // Divide members among orbits evenly
+            const orbitMembers = members.filter(
+               (_, idx) => idx % 4 === orbitIndex
+            );
+
+            return (
+               <div
+                  key={orbitIndex}
+                  className={`orbit orbit-${orbitIndex + 1}`}
+               >
+                  {orbitMembers.map((member, memberIndex) => {
+                     // Random angle ensuring no overlaps
+                     const angle = getRandomInRange(1, 2) * 360; // Random number between 1 and 100
+
+                     const radians = (angle * Math.PI) / 180;
+
+                     // Calculate x, y positions for circular placement
+                     const radius = 50 + orbitIndex * 50; // Orbit radius
+                     const x = Math.cos(radians) * radius;
+                     const y = Math.sin(radians) * radius;
+
+                     const style = {
+                        transform: `translate(${x}px, ${y}px)`,
+                     };
+
+                     return (
+                        <div key={memberIndex} className="avatar" style={style}>
+                           <img src={member.avatar} alt={member.name} />
+                        </div>
+                     );
+                  })}
+               </div>
+            );
+         })}
       </div>
-   )
-}
+   );
+};
 
 export default Feedbacks;
