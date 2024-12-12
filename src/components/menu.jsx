6ff5b7df1a2fea39432dd54163329f0bb0814d19
DonJuan <armvnov1ch@gmail.com>
2024-12-12 22:07:09 +0500

    menu responsive

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,38 +1,53 @@
-import React from "react";const Menu = () => {
-   return (
-      <div id="menu">
-         <div className="container">
-            <div className="menu">
-               <div className="logo">
-                  <img src="img/logo.png" alt="" />
-               </div>
-               <div className="navbar">
-                  <a id="white" href="#about">
-                     Главная
-                  </a>
-                  <a id="white" href="#counters">
-                     Про нас
-                  </a>
-                  <a id="white" href="#cases">
-                     Кейсы
-                  </a>
-                  <a id="white" href="#services">
-                     Услуги
-                  </a>
-                  <a id="white" href="#feedbacks">
-                     Отзывы
-                  </a>
-                  <a id="white" href="#members">
-                     Сотрудники
-                  </a>
-                  <a id="white" href="#footer">
-                     Контакты
-                  </a>
-               </div>
-            </div>
-         </div>
+import React, { useState } from "react";
+
+const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div id="menu">
+      <div className="container">
+        <div className="menu">
+          <div className="logo">
+            <img src="img/logo.png" alt="Logo" />
+          </div>
+          <div className={`navbar ${isMenuOpen ? "open" : ""}`}>
+            <a id="white" href="#about">
+              Главная
+            </a>
+            <a id="white" href="#counters">
+              Про нас
+            </a>
+            <a id="white" href="#cases">
+              Кейсы
+            </a>
+            <a id="white" href="#services">
+              Услуги
+            </a>
+            <a id="white" href="#feedbacks">
+              Отзывы
+            </a>
+            <a id="white" href="#members">
+              Сотрудники
+            </a>
+            <a id="white" href="#footer">
+              Контакты
+            </a>
+          </div>
+          <button
+            className="menu-toggle"
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+          >
+            ☰
+          </button>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Menu;
