491e3c2773709bb556fd51572d0da05206c8409d
DonJuan <armvnov1ch@gmail.com>
2024-12-17 17:36:29 +0500

    menu services

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,53 +1,81 @@
-import React, { useState } from "react";
+import React, { useState } from "react";const Menu = () => {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const [isServicesOpen, setIsServicesOpen] = useState(false);
 
-const Menu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+   };
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+   const toggleServices = () => {
+      setIsServicesOpen(!isServicesOpen);
+   };
 
-  return (
-    <div id="menu">
-      <div className="container">
-        <div className="menu">
-          <div className="logo">
-            <img src="img/logo.png" alt="Logo" />
-          </div>
-          <div className={`navbar ${isMenuOpen ? "open" : ""}`}>
-            <a id="white" href="#about">
-              Главная
-            </a>
-            <a id="white" href="#counters">
-              Про нас
-            </a>
-            <a id="white" href="#cases">
-              Кейсы
-            </a>
-            <a id="white" href="#services">
-              Услуги
-            </a>
-            <a id="white" href="#feedbacks">
-              Отзывы
-            </a>
-            <a id="white" href="#members">
-              Сотрудники
-            </a>
-            <a id="white" href="#footer">
-              Контакты
-            </a>
-          </div>
-          <button
-            className="menu-toggle"
-            onClick={toggleMenu}
-            aria-label="Toggle Menu"
-          >
-            ☰
-          </button>
-        </div>
+   return (
+      <div id="menu">
+         <div className="container">
+            <div className="menu">
+               <div className="logo">
+                  <img src="img/logo.png" alt="Logo" />
+               </div>
+               <div className={`navbar ${isMenuOpen ? "open" : ""}`}>
+                  <a id="white" href="#about">
+                     Главная
+                  </a>
+                  <a id="white" href="#counters">
+                     Про нас
+                  </a>
+                  <a id="white" href="#cases">
+                     Кейсы
+                  </a>
+
+                  {/* Services with Subnavigation */}
+                  <div className="services-dropdown">
+                     <a
+                        id="white"
+                        href="#services"
+                        onClick={(e) => {
+                           e.preventDefault();
+                           toggleServices();
+                        }}
+                     >
+                        Услуги
+                     </a>
+                     {isServicesOpen && (
+                        <div className="sub-menu">
+                           <a id="white" href="#first">
+                              CEO
+                           </a>
+                           <a id="white" href="#second">
+                              Разработка
+                           </a>
+                           <a id="white" href="#third">
+                              Маркетинг
+                           </a>
+                        </div>
+                     )}
+                  </div>
+
+                  <a id="white" href="#feedbacks">
+                     Отзывы
+                  </a>
+                  <a id="white" href="#members">
+                     Сотрудники
+                  </a>
+                  <a id="white" href="#footer">
+                     Контакты
+                  </a>
+               </div>
+               <button
+                  className="menu-toggle"
+                  onClick={toggleMenu}
+                  aria-label="Toggle Menu"
+               >
+                  ☰
+               </button>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Menu;
